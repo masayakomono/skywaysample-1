eada32ea123a0c39bcb952c5ace407ebe3e53cc8
bbholic <105488045+bbholic@users.noreply.github.com>
2022-05-14 13:12:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const Peer = window.Peer;
 
   // eslint-disable-next-line require-atomic-updates
   const peer = (window.peer = new Peer({
-    key: window.__SKYWAY_KEY__,
+    key: window.__SKYWAY_KEY__ = '530f6754-b7f3-4d51-8fb0-3f922a655f00',
     debug: 3,
   }));
 
